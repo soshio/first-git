8215bed78e1211ecb61765317629f29b96a6e831
S.S <tectest58@gmail.com>
2018-06-08 03:59:27 +0000

    add column

--- database/migrations/2018_06_08_061033_create_messages_table.php
+++ database/migrations/2018_06_08_061033_create_messages_table.php
@@ -16,6 +16,7 @@ class CreateMessagesTable extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
             $table->string('content');    // content カラム追加
+            $table->int('number');
             $table->timestamps();
         });
     }
